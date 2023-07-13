47e2122c15d85c954fa5535cf940e716da8fac86
Draraedus <adriel2002santos@gmail.com>
2023-07-13 20:38:49 -0300

    1.1.6 colors palette modified

--- scripts/aditionalContentsRender.js
+++ scripts/aditionalContentsRender.js
@@ -11,7 +11,7 @@ function noUser404() {
 
 function loading() {
     document.getElementsByClassName('dataContainer')[0].innerHTML = `
-    <svg class="loadingIcon" xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="orange" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+    <svg class="loadingIcon" xmlns="http://www.w3.org/2000/svg" width="160" height="160" fill="#23CE6B" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
         <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
     </svg>
